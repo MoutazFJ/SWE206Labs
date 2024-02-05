50d9e2a630459f6ce013a4c173449a725c7be052
MoutazFJ <moutazbusiness@gmail.com>
2024-02-05 15:32:34 +0300

    Added selectionSort

--- Sorter.java
+++ Sorter.java
@@ -1,2 +1,19 @@
-public class Sorter {
+public class Sorter<T extends Comparable<T>> {
+    public void selectionSort(T[] array) {
+
+        // Traverse through all array elements
+        for (int i = 0; i < array.length - 1; i++) {
+            // Find the minimum element in unsorted array
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j].compareTo(array[minIndex]) < 0) {
+                    minIndex = j;
+                }
+            }
+            // Swap the found minimum element with the first element
+            T temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
 }
